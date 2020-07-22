5a9bf0e82e6a60801c1051c3091308e043ff5ded
Timothy Collins <timothycollins.sean@gmail.com>
2020-07-22 16:53:26 +0500

    Fetched data and Store data for products using MySql

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,16 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows,fieldData])=>{
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
-    });
-  });
+})
+  })
+  .catch(err=>console.log(err))
+    
 };
 
 exports.getProduct = (req, res, next) => {
@@ -23,12 +26,14 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows,fieldData])=>{
     res.render('shop/index', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Shop',
       path: '/'
-    });
+  })
+  .catch(err=>console.log(err))
   });
 };
 
