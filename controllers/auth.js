8d6fbd76ba8c7f3db754197966136691c23d6cd3
Timothy Collins <timothycollins.sean@gmail.com>
2021-07-16 11:15:00 +0500

    Added cookie to enable admin routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,14 @@
 exports.getLogin = (req, res, next) => {
-
+        const isLoggedIn = req.get('Cookie').split(';')[1].trim().split('=')[1];
         res.render('auth/login', {
           path: '/login',
           pageTitle: 'Login',
-          isAuthenticated: req.isLoggedIn
+          isAuthenticated: isLoggedIn
         });
   };
 
   exports.postLogin = (req,res,next)=>{
-      req.isLoggedIn = true;
+      res.setHeader('Set-Cookie', 'loggedIn=true');
       res.redirect('/');
   }
   
