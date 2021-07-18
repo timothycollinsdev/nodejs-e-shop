0e19605905c5fa1b9dcea8087925a5835e23e4c4
Timothy Collins <timothycollins.sean@gmail.com>
2021-07-18 09:58:27 +0500

    Encrypting passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs'); 
 
 exports.getLogin = (req, res, next) => {
         res.render('auth/login', {
@@ -41,9 +42,12 @@ exports.postSignup = (req, res, next) => {
       if(userDoc){
         return res.redirect('/signup');
       }
+      return bcrypt.hash(password, 12);
+    })
+    .then(hashedPassword =>{
       const user = new User({
         email: email,
-        password: password,
+        password: hashedPassword,
         cart: {items:[]}
       });
       return user.save();
