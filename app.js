1134454e325f20a7033b889e0c701eeb8882d41b
Timothy Collins <timothycollins.sean@gmail.com>
2021-07-18 12:37:58 +0500

    Used a CSRF token

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-
+const csrf = require('csurf');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
@@ -17,6 +17,8 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 
+const csrfProtection = csrf();
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -27,6 +29,8 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
+
+app.use(csrfProtection);
 
 app.use((req, res, next) => { 
   if(!req.session.user){
