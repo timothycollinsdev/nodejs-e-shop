2fe9975528e781f24d16d53c319078cc49881af8
Timothy Collins <timothycollins.sean@gmail.com>
2021-07-18 11:12:20 +0500

    Working on route protection

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,9 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
+  if(!req.session.isLogged){
+    return res.redirect('/login');
+  }
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
