e6f2b7a9712c5c2d5336ff8895b2702012913d32
Timothy Collins <timothycollins.sean@gmail.com>
2021-07-26 23:11:54 +0500

    Added validation for email using express-validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs'); 
 const crypto = require('crypto');
 const nodemailer = require("nodemailer");
+const {validationResult} = require('express-validator/check')
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
@@ -83,6 +84,14 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password  = req.body.password;
     const confirmPassword = req.body.confirmpassword;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(422).render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        errorMessage: errors.array()
+      });
+    }
     User.findOne({email: email})
     .then(userDoc =>{
       if(userDoc){
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -9,7 +10,7 @@ router.post('/login', authController.postLogin);
 
 router.post('/logout', authController.postLogout);
 
-router.get('/signup', authController.getSignup);
+router.get('/signup', check('email').isEmail(), authController.getSignup);
 router.post('/signup', authController.postSignup);
 
 router.get('/reset', authController.getReset);
