b5af4d20d6a0ebe8f7c99728e37aea98f39eb78c
Timothy Collins <timothycollins.sean@gmail.com>
2021-07-27 13:33:55 +0500

    Added error message for invalid email and novalidate for frontend error message

--- routes/auth.js
+++ routes/auth.js
@@ -10,8 +10,8 @@ router.post('/login', authController.postLogin);
 
 router.post('/logout', authController.postLogout);
 
-router.get('/signup', check('email').isEmail(), authController.getSignup);
-router.post('/signup', authController.postSignup);
+router.get('/signup', authController.getSignup);
+router.post('/signup', check('email').isEmail(), authController.postSignup);
 
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
