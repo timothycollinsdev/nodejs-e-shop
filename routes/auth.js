b7ae011ec162bcd6d8501a32c2c853bdee595672
Timothy Collins <timothycollins.sean@gmail.com>
2021-07-28 18:44:20 +0500

    Added validation for signup email and password along with forbidden email restriction

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check,body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -11,7 +11,19 @@ router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 
 router.get('/signup', authController.getSignup);
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email'), authController.postSignup);
+router.post('/signup',
+    [
+    check('email')
+    .isEmail()
+    .withMessage('Please enter a valid email')
+    .custom((value,{req})=>{
+        if(value==='admin@admin.com'){
+            throw new Error('This email address is not allowed');
+        }
+        return true;
+    }),
+    body('password','Please enter a password with 5 numbers').isLength({min:5}).isAlphanumeric()   
+], authController.postSignup);
 
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
