6d6dc1be27002d2227242c7978b5b4b23781b6da
Timothy Collins <timothycollins.sean@gmail.com>
2021-08-01 20:01:06 +0500

    Added old data while signing up to avoid losing entered user data once error arrives

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,6 +28,16 @@ exports.getLogin = (req, res, next) => {
   exports.postLogin = (req,res,next)=>{
     const email = req.body.email;
     const password = req.body.password;
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+      return res.status(422).render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        errorMessage: errors.array()[0].msg
+      });
+    }
+
     User.findOne({email: email})
     .then(user=>{
       if(!user){
@@ -69,7 +79,12 @@ exports.getLogin = (req, res, next) => {
     res.render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: message
+      errorMessage: message,
+      oldInput: {
+        email: "", 
+        password: "", 
+        confirmpassword: "" 
+      }
     });
 };
 
@@ -81,7 +96,12 @@ exports.postSignup = (req, res, next) => {
       return res.status(422).render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        errorMessage: errors.array()[0].msg
+        errorMessage: errors.array()[0].msg,
+        oldInput: { 
+          email: email, 
+          password: password, 
+          confirmpassword: req.body.confirmpassword 
+        }
       });
     }
      bcrypt
