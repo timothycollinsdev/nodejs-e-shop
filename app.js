6245b6b6281413fd46e8c7e77de5dd30de6af5e4
Timothy Collins <timothycollins.sean@gmail.com>
2021-08-10 22:44:37 +0500

    Added error middleware to catch block in case of internal server error

--- app.js
+++ app.js
@@ -61,6 +61,9 @@ app.use(authRoutes);
 
 app.use('/500', errorController.get500);
 app.use(errorController.get404);
+app.use((error, req, res, next)=>{
+  res.redirect('/500');
+});
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true } )
 .then(result=>{
--- controllers/admin.js
+++ controllers/admin.js
@@ -56,7 +56,9 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect('/admin/products');
     })
     .catch(err => {
-      res.redirect('/500');
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -87,7 +89,11 @@ exports.getEditProduct = (req, res, next) => {
         validationErrors: []
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postEditProduct = (req, res, next) => {
