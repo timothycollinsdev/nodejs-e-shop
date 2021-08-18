7b9bc38b58abe888d2839c3f70a025fac17d4fa6
Timothy Collins <timothycollins.sean@gmail.com>
2021-08-18 14:08:44 +0500

    Sending and handling backend requests

--- controllers/admin.js
+++ controllers/admin.js
@@ -176,8 +176,8 @@ exports.getProducts = (req, res, next ) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {   
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {   
+  const prodId = req.params.productId;
   Product.findById(prodId).then(product=>{
     if(!product){
       return next(new Error('Product not found!'))
@@ -186,11 +186,9 @@ exports.postDeleteProduct = (req, res, next) => {
     return Product.deleteOne({_id: prodId, userId: req.user._id});
   })
   .then(() => {
-    res.redirect('/admin/products');
+    res.status(200).json({message: 'Success!'});
   })
   .catch(err =>{
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
+    res.status(500).json({message: 'Deleting product failed.'});
   });
 };
