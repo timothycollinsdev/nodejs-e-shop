5e063a26450fb9d727dc86543a74508864e3f89c
Timothy Collins <timothycollins.sean@gmail.com>
2021-08-18 15:28:09 +0500

    manipulating the dom and deleting product root element to delete it without refreshing the page

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,12 +2,20 @@ const deleteProduct = (btn) =>{
    const prodId = btn.parentNode.querySelector('[name=productId]').value;
    const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
 
+   const prodElement = btn.closest('article');
+
    fetch('/admin/product/'+ prodId, {
       method: 'DELETE',
       headers: {
          'csrf-token': csrf
       }
    })
-   .then(result=>console.log(result))
+   .then(result=>{
+      return result.json();
+   })
+   .then(data=>{
+      console.log(data);
+      prodElement.parentNode.removeChild(prodElement);
+   })
    .catch(err=>console.log(err));
 };
